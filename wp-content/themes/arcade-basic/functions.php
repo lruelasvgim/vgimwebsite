94ce9a2a3d8827af16d485496e615f34cf165d86
lruelasvgim <luis.ruelas@vgim.com.mx>
2016-01-28 13:30:14 -0600

    upload images

--- wp-content/themes/arcade-basic/functions.php
+++ wp-content/themes/arcade-basic/functions.php
@@ -680,3 +680,11 @@ function bavotasan_wrapper_end() {
 	</div>
 	<?php
 }
+//Allow Contributors to Add Media
+if ( current_user_can('contributor') && !current_user_can('upload_files') )
+add_action('admin_init', 'allow_contributor_uploads');
+  
+function allow_contributor_uploads() {
+     $contributor = get_role('contributor');
+     $contributor->add_cap('upload_files');
+}
